0735ff3216078360b649bff455b075d757243073
yoyo930021 <yoyo930021@gmail.com>
2020-10-31 18:46:49 +0800

    Add display setting mode button sheet

--- src/components/SwitchMapModeButton.vue
+++ src/components/SwitchMapModeButton.vue
@@ -2,13 +2,15 @@
   <v-bottom-sheet v-model="mapModeBottomSheet">
     <template v-slot:activator="{ on, attrs }">
       <v-btn
-        rounded
+        :rounded="!isInSheet"
+        :text="isInSheet"
+        :class="isInSheet ? 'pa-0' : null"
         color="white"
         class="primary--text"
         v-bind="attrs"
         v-on="on"
       >
-        切換地圖模式•簡易地圖
+        切換地圖模式 • {{currentMapModeName}}
       </v-btn>
     </template>
     <v-list>
@@ -16,7 +18,7 @@
       <v-list-item
         v-for="mode in mapModes"
         :key="mode.type"
-        @click="clickChangeBaseLayer(mode)"
+        @click="currentMapMode = mode.type"
       >
         <v-list-item-title class="primary--text">{{ mode.name }}</v-list-item-title>
       </v-list-item>
@@ -25,32 +27,48 @@
 </template>
 
 <script lang="ts">
-import { createComponent, ref, inject } from '@vue/composition-api'
+import { createComponent, ref, inject, computed } from '@vue/composition-api'
 import { BASE_MAP, BASE_MAP_NAME, MapFactoryController } from '@/lib/map'
 import { MainMapControllerSymbol } from '@/symbols'
+import { useMapMode } from '@/lib/useMapMode'
 
 export default createComponent({
-  name: 'SwitchMapModeButto',
+  name: 'SwitchMapModeButton',
+  props: {
+    isInSheet: {
+      type: Boolean,
+      default: false
+    }
+  },
   setup () {
     const mapModeBottomSheet = ref(false)
     const mapController = inject(MainMapControllerSymbol, ref<MapFactoryController>())
+
+    const { currentMapMode: currentMapMode$ } = useMapMode()
+    const currentMapMode = computed({
+      get: () => currentMapMode$.value,
+      set: (value) => {
+        currentMapMode$.value = value
+
+        mapController.value?.mapInstance.changeBaseMap(value)
+        mapModeBottomSheet.value = false
+      }
+    })
 
     const mapModes = [BASE_MAP.SATELITE, BASE_MAP.OSM, BASE_MAP.TAIWAN].map(type => ({
       type,
       name: BASE_MAP_NAME[type]
     }))
-    const clickChangeBaseLayer = (mode: { type: BASE_MAP, name: string }) => {
-      if (mapController.value) {
-        mapController.value?.mapInstance.changeBaseMap(mode.type)
 
-        mapModeBottomSheet.value = false
-      }
-    }
+    const currentMapModeName = computed(() =>
+      mapModes.find((mode) => mode.type === currentMapMode.value)?.name ?? mapModes[0]?.name
+    )
 
     return {
       mapModeBottomSheet,
       mapModes,
-      clickChangeBaseLayer
+      currentMapMode,
+      currentMapModeName
     }
   }
 })
