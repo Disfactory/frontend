2bc63e3af960d4cad52f7535cf91b6530f88b3bd
Link1515 <h26281660@gmail.com>
2025-10-13 15:43:08 +0800

    refactor: migrate ContactModal to use Vuetify dialog

--- src/App.vue
+++ src/App.vue
@@ -117,7 +117,7 @@
       </v-dialog>
 
       <about-modal :open="modalState.aboutModalOpen" :dismiss="modalActions.closeAboutModal" />
-      <contact-modal :open="modalState.contactModalOpen" :dismiss="modalActions.closeContactModal" />
+      <contact-modal v-model="modalState.contactModalOpen" />
       <getting-started-modal :open="modalState.gettingStartedModalOpen" :dismiss="modalActions.closeGettingStartedModal" />
       <safety-modal v-model="modalState.safetyModalOpen" />
       <tutorial-modal v-model:open="modalState.tutorialModalOpen" />
